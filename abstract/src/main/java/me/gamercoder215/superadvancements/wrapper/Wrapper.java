84f62d516623b35cfe2ad834ef837623200ec4ae
GamerCoder <gamercoder215@teaminceptus.us>
2023-05-12 14:19:14 +0000

    Remove Unused Wrapper#getRecipe

--- abstract/src/main/java/me/gamercoder215/superadvancements/wrapper/Wrapper.java
+++ abstract/src/main/java/me/gamercoder215/superadvancements/wrapper/Wrapper.java
@@ -1,7 +1,29 @@
 package me.gamercoder215.superadvancements.wrapper;
 
+import org.bukkit.Bukkit;
+
 public interface Wrapper {
 
+    Wrapper w = getWrapper();
+
     String PACKET_INJECTOR_ID = "superadvancements:packet_injector";
+
+    // Static Util
+
+    static String getServerVersion() {
+        return Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3].substring(1);
+    }
+
+    static Wrapper getWrapper() {
+        String v = getServerVersion();
+        try {
+            return Class.forName("me.gamercoder215.superadvancements.v" + v + ".Wrapper" + v)
+                .asSubclass(Wrapper.class)
+                .getConstructor()
+                .newInstance();
+        } catch (ReflectiveOperationException e) {
+            throw new IllegalStateException("Unknown Wrapper Version: " + v, e);
+        }
+    }
 
 }
