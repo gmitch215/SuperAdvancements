a23fd36460190af6cd580747a56021547fdbf2c8
GamerCoder <gamercoder215@teaminceptus.us>
2023-05-12 14:17:09 +0000

    Create Wrapper1_12_R1.java

---
+++ nms/1_12_R1/src/main/java/me/gamercoder215/superadvancements/v1_12_R1/Wrapper1_12_R1.java
@@ -0,0 +1,14 @@
+package me.gamercoder215.superadvancements.v1_12_R1;
+
+import me.gamercoder215.superadvancements.wrapper.Wrapper;
+
+import net.minecraft.server.v1_12_R1.*;
+
+import org.bukkit.Keyed;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.Recipe;
+import org.bukkit.craftbukkit.v1_12_R1.util.CraftNamespacedKey;
+
+public final class Wrapper1_12_R1 implements Wrapper {
+
+}
